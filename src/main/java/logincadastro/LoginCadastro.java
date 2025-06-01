5bc401d35b5db66287fbca09bfb39c5db8088703
Luiz <Luiz4ugustoPereira@gmail.com>
2025-06-01 19:12:39 -0300

    Commit Cadastrar Categoria e Cadastrar Produto com integração no banco de dados

---
+++ src/main/java/logincadastro/LoginCadastro.java
@@ -0,0 +1,8 @@
+package logincadastro;
+
+public class LoginCadastro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+import visao.FrmLogin;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmLogin objetotela = new FrmLogin();
+        objetotela.setVisible(true);
+    }
+}
